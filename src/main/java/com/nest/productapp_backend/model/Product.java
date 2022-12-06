037b71fba62210d23d4265670f73ea57ffc868b7
AmalAlby <amalalbyn@gmail.com>
2022-12-06 16:05:26 +0530

    crossorigin added to product api

--- src/main/java/com/nest/productapp_backend/model/Product.java
+++ src/main/java/com/nest/productapp_backend/model/Product.java
@@ -1,10 +1,7 @@
 package com.nest.productapp_backend.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name="products")
